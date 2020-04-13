48c77578e294b0569074d919a8c53d3340ef3504
guina.costa <agnaldo.almeida@iteris.com.br>
2020-04-13 02:11:21 -0300

    configuração das RouterFunctions

--- src/main/java/br/com/meetups/funcional/repository/UserRepository.java
+++ src/main/java/br/com/meetups/funcional/repository/UserRepository.java
@@ -1,7 +1,37 @@
 package br.com.meetups.funcional.repository;
 
+import br.com.meetups.funcional.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Repository
 public class UserRepository {
+    private final WebClient webClient = WebClient.create("http://www.mocky.io/v2");
+
+    private static final String ID_PATH = "/{id}";
+
+    public Mono<User> findUser(String id) {
+        return webClient.get()
+                .uri(ID_PATH, id) //"5e93d1c83000001f5a156c48"
+                .retrieve()
+                .bodyToMono(User.class);
+    }
+
+    public Flux<User> findAllUsers() {
+        return webClient.get()
+                .uri(ID_PATH, "5e93d52a3000007400156c56")
+                .retrieve()
+                .bodyToFlux(User.class);
+    }
+
+    public Flux<User> findManyUsers() {
+        return webClient.get()
+                .uri(ID_PATH, "5e93df2a3000007400156c8b")
+                .retrieve()
+                .bodyToFlux(User.class);
+    }
 }
--- src/main/java/br/com/meetups/funcional/resource/UserResource.java
+++ src/main/java/br/com/meetups/funcional/resource/UserResource.java
@@ -1,7 +1,35 @@
 package br.com.meetups.funcional.resource;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.meetups.funcional.model.User;
+import br.com.meetups.funcional.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
-@RestController
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@RequiredArgsConstructor
 public class UserResource {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    RouterFunction<ServerResponse> getAllUsersRoute() {
+        return RouterFunctions.route(
+                GET("/users"),
+                request -> ServerResponse.ok().body(BodyInserters.fromValue(userRepository.findAllUsers()))
+        );
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> getUserById() {
+        return RouterFunctions.route(
+                GET("/users/{id}"),
+                request -> ServerResponse.ok().body(userRepository.findUser(request.pathVariable("id")), User.class)
+        );
+    }
+
 }
